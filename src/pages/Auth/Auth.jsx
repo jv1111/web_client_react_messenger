e98bf75e2a989b7705d2b4c2f5a4a1686a606418
JeremyV1111 <webd0829@gmail.com>
2023-10-09 23:58:56 -0700

    submit form

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -1,5 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import FormTextField from "../../components/FormTextField";
+
+const LoginForm = () => {
+
+    const [usernameOrEmail, setUsernameOrEmail] = useState("");//"" is the default value
+    const [password, setPassword] = useState("");
+
+    const submitHandler = async (event) => {
+        alert("sub");
+    }
+
+    return(
+        <form className="authForm" onSubmit={submitHandler} >
+            <h2 className="labelTitle">Login</h2>
+            <FormTextField
+                type="text"
+                name="usernameOrEmail"
+                label="Username or Email"
+                onChange={(event)=>setUsernameOrEmail(event.target.value)}
+                value={usernameOrEmail}
+            />
+            <FormTextField
+                type="password"
+                name="password"
+                label="Password"
+                onChange={(event)=>setPassword(event.target.value)}
+                value={password}
+            />
+            <button className="btnPrimary" type="submit"> Login </button>
+            <button className="btnSecondary" type="button"> Sign up </button>
+        </form>
+    );
+}
 
 const AuthPage = () => {
     return(
@@ -8,23 +40,7 @@ const AuthPage = () => {
                 <h1 className="title">Messenger</h1>
                 <p className="titel-description">Welcome to messenger</p>
             </div>
-            <form
-                className="authForm"
-            >
-                <h2 className="labelTitle">Login</h2>
-                <FormTextField
-                    type="text"
-                    name="usernameOrEmail"
-                    label="Username or Email"
-                />
-                <FormTextField
-                    type="password"
-                    name="password"
-                    label="Password"
-                />
-                <button className="btnPrimary" type="submit"> Login </button>
-                <button className="btnSecondary" type="button"> Sign up </button>
-            </form>
+            {<LoginForm/>}
         </div>
     )
 }
